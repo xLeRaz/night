91e6f661bb7c0fd41c1cd25c958fe877b43f17b2
xLeRaz <40789305+xLeRaz@users.noreply.github.com>
2018-09-17 10:35:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,13 +4,11 @@ const moment = require('moment');
 const ms = require('ms');
 const fs = require('fs');
 const night = new Discord.Client({disableEveryone: true, maxMessagesCache: 1});
-const format = ['st', 'nd', 'rd', 'th']
 
 night.on('ready',async () => {
   console.log(`.Servers: ${night.guilds.size}`);
   if(night.guild.size === 0) return night.generateInvite(['ADMINISTRATOR']).then(i => console.log(i));
 });
 
-night.on('guildCreate', g => console.log(`${format[night.guilds.size]} Server.`));
 
 night.login(process.env.NIGHT);
